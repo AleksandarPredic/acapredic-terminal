94ae33ab879bac6fe11711582c1aa2eb53994ed3
Aca <aleksandar.predic@gmail.com>
2019-02-20 18:28:34 +0100

    Changed description index.html

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -13,7 +13,7 @@
 
 // https://github.com/sasadjolic/dom-terminal
 // https://github.com/AleksandarPredic/dom-terminal
-import Terminal from "../vendor/dom-terminal/dist/terminal-1.0.2.min";
+import Terminal from "../vendor/dom-terminal/terminal-1.0.2.min";
 import info from './info';
 import isIOS from './isIos';
 
